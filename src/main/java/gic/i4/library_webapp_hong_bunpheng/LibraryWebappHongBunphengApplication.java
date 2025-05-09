dbbf4ee92baa8f93fae2428f9c4d6bfecdc03bb6
HongBunpheng <hongbunpheng89@gmail.com>
2025-05-09 11:12:28 +0700

    midterm exam

---
+++ src/main/java/gic/i4/library_webapp_hong_bunpheng/LibraryWebappHongBunphengApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_hong_bunpheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappHongBunphengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappHongBunphengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_hong_bunpheng/LibraryWebappHongBunphengApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_hong_bunpheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappHongBunphengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
